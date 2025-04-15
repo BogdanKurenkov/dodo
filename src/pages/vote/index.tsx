e4aa19c31025a693595e94a35c2b12e334b29173
bogdankurenkov <badikradik123@gmail.com>
2025-04-15 12:30:55 +0300

    added vote page

---
+++ src/pages/vote/index.tsx
@@ -0,0 +1,15 @@
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { FC } from "react";
+
+export default function Vote() {
+    return <></>
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale ?? 'ru', ['common'])),
+        },
+    };
+};
