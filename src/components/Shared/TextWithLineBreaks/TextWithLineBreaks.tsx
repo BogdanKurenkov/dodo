5494b6ac6ce063c1f6ac7bf2f0913f2dcf434a99
bogdankurenkov <badikradik123@gmail.com>
2025-04-29 17:53:14 +0300

    added check for undefined

--- src/components/Shared/TextWithLineBreaks/TextWithLineBreaks.tsx
+++ src/components/Shared/TextWithLineBreaks/TextWithLineBreaks.tsx
@@ -7,7 +7,7 @@ interface ITextWithLineBreaks {
 export const TextWithLineBreaks: React.FC<ITextWithLineBreaks> = ({ text }) => {
     return (
         <>
-            {text.split('\n').map((line, i, arr) => (
+            {text?.split('\n').map((line, i, arr) => (
                 <React.Fragment key={i}>
                     {line}
                     {i < arr.length - 1 && <br />}
