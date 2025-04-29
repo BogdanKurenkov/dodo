9fc51d51d0a439f4b119e8c51c028739eb8135f3
bogdankurenkov <badikradik123@gmail.com>
2025-04-29 18:11:46 +0300

    vote test

---
+++ src/pages/vote/index.tsx
@@ -0,0 +1,177 @@
+// import { useState } from "react";
+import { GetServerSideProps } from "next";
+// import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import { Footer } from "@/components/Footer/Footer";
+import { Header } from "@/components/Header/Header";
+// import { Container } from "@/components/Shared/Container/Container";
+// import { SectionTitle } from "@/components/Shared/SectionTitle/SectionTitle";
+// import { TextWithLineBreaks } from "@/components/Shared/TextWithLineBreaks/TextWithLineBreaks";
+// import { LottieBase } from "@/components/LottieBase/LottieBase";
+// import { LottieRotate } from "@/components/LottieRotate/LottieRotate";
+
+// import {
+//     VoteBackground,
+//     SaucesList,
+//     SauceContainer,
+//     SauceSample,
+//     SauceTitle,
+//     Button,
+//     VotePrompt,
+// } from "./styled";
+// import { useRouter } from "next/navigation";
+
+// const sauces = [
+//     "sauces.sauce1.name",
+//     "sauces.sauce2.name",
+//     "sauces.sauce3.name"
+// ];
+
+// const animations_open = [
+//     <LottieBase key={1} path="/lottie/vote/dip_1_3_opening_lottie/animation.json" width={200} height={200} />,
+//     <LottieBase key={2} path="/lottie/vote/dip_2_2_opening_lottie/animation.json" width={200} height={200} />,
+//     <LottieBase key={3} path="/lottie/vote/dip_3_2_opening_lottie/animation.json" width={200} height={200} />
+// ]
+
+export default function Vote() {
+    // const { t } = useTranslation('common');
+
+    // const router = useRouter();
+
+    // const [activeCard, setActiveCard] = useState<number | null>(null);
+    // const [isButtonActive, setIsButtonActive] = useState(false);
+    // const [isPlaying, setIsPlaying] = useState(false);
+    // const [step, setStep] = useState(1);
+
+    // const animations_rotate = [
+    //     <LottieRotate key={4} isAnimate={!isPlaying} isPlaying={isPlaying} path="/lottie/vote/dip_1_3_rotation_lottie/animation.json" width={200} height={200} />,
+    //     <LottieRotate key={5} direction="down" isAnimate={!isPlaying} isPlaying={isPlaying} path="/lottie/vote/dip_3_3_rotation_lottie/animation.json" width={200} height={200} />,
+    //     <LottieRotate key={6} isAnimate={!isPlaying} isPlaying={isPlaying} path="/lottie/vote/dip_3_2_rotation_lottie/animation.json" width={200} height={200} />
+    // ];
+
+    // const handleCardClick = (number: number) => {
+    //     if (activeCard === number) {
+    //         setActiveCard(null);
+    //         setIsButtonActive(false);
+    //     } else {
+    //         setActiveCard(number);
+    //         setIsButtonActive(true);
+    //     }
+    // };
+
+    // const handleNextStep = () => {
+    //     if (step === 1) {
+    //         setIsPlaying(true);
+    //         setTimeout(() => {
+    //             setStep(2);
+    //         }, 1000);
+    //     }
+    // };
+
+    // const handleVoteClick = () => {
+    //     if (step === 1) {
+    //         setIsPlaying(true);
+    //         setTimeout(() => {
+    //             setStep(2);
+    //         }, 1000);
+    //     } else {
+    //         router.push('/voteResult')
+    //     }
+
+    // }
+
+    return (
+        <>
+            <Header />
+            {/* <main className="main">
+                <VoteBackground>
+                    <Container>
+                        <SectionTitle isWhite={true}>
+                            <TextWithLineBreaks text={t('vote.title')} />
+                        </SectionTitle>
+                        <SaucesList>
+                            {sauces.map((sauce, index) => (
+                                <SauceContainer key={index}>
+                                    <div style={{ position: 'relative', width: '200px', height: '200px' }}>
+                                        <div
+                                            style={{
+                                                position: 'absolute',
+                                                top: 0,
+                                                left: 0,
+                                                opacity: step === 1 ? 1 : 0,
+                                                pointerEvents: step === 1 ? 'auto' : 'none',
+                                                transition: 'opacity 0.3s ease'
+                                            }}
+                                            onClick={handleNextStep}
+                                        >
+                                            {animations_rotate[index]}
+                                        </div>
+
+                                        <div
+                                            style={{
+                                                position: 'absolute',
+                                                top: 0,
+                                                left: 0,
+                                                opacity: step === 2 ? 1 : 0,
+                                                pointerEvents: step === 2 ? 'auto' : 'none',
+                                                transition: 'opacity 0.3s ease'
+                                            }}
+                                            onClick={() => handleCardClick(index)}
+                                        >
+                                            {animations_open[index]}
+                                        </div>
+                                    </div>
+
+                                    <SauceSample>{t('results.sample')} № {index + 1}</SauceSample>
+                                    <SauceTitle>{t(sauce)}</SauceTitle>
+                                </SauceContainer>
+                            ))}
+                        </SaucesList>
+                        <Button
+                            $variant="glass"
+                            disabled={step === 2 && !isButtonActive}
+                            type="button"
+                            onClick={handleVoteClick}
+                        >
+                            {step === 2 ? t('buttons.vote_select') : t('buttons.start')}
+                        </Button>
+                        <VotePrompt style={{ opacity: step === 1 ? '0' : '1' }}>{t('vote.click')}</VotePrompt>
+                    </Container>
+                </VoteBackground>
+            </main> */}
+
+            <Footer />
+        </>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { req, query } = context;
+    const { source } = query;
+
+    const cookies = req.headers.cookie?.split(';').reduce((acc, cookie) => {
+        const [key, value] = cookie.trim().split('=');
+        acc[key] = value;
+        return acc;
+    }, {} as Record<string, string>) || {};
+
+    // TODO потом убрать заглушку и привязатсья к реальной куке
+
+    const accessToken = cookies.accessToken || true;
+
+    if (!accessToken && source === 'qr') {
+        return {
+            redirect: {
+                destination: '/auth?source=qr',
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {
+            ...(await serverSideTranslations(context.locale ?? "ru", ["common"])),
+        },
+    };
+};
